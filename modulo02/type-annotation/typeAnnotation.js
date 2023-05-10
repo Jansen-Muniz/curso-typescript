30105426acbd101dc452eec15fda55b756b10a42
Jansen Muniz <92958989+Jansen-Muniz@users.noreply.github.com>
2023-05-09 21:57:01 -0300

    type annotation

---
+++ modulo02/type-annotation/typeAnnotation.js
@@ -0,0 +1,16 @@
+"use strict";
+// => Variáveis [Type Annotations]
+let nome = 'Jansen Muniz';
+console.log(nome);
+// => Arrays [Type Annotations]
+let animais = ['Elefante', 'Cachorro', 'Gato', 'Panda', 'Girafa'];
+console.log(animais);
+// => Objeto
+let carro;
+carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 };
+console.log(carro);
+// => Funções
+function multiplicarNumeros(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicarNumeros(2, 5));
