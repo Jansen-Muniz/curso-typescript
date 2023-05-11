58a5e342e550a33195cad8ae0f73cbe1466cc950
Jansen Muniz <92958989+Jansen-Muniz@users.noreply.github.com>
2023-05-10 21:22:38 -0300

    number e bigint

---
+++ modulo02/12-aula-number-bigint/number-bigint.js
@@ -0,0 +1,19 @@
+"use strict";
+// => Exemplo: number
+let num1 = 23.0;
+let num2 = 0x78CF;
+let num3 = 0o577;
+let num4 = 0b110001;
+console.log('Number - Ponto Flutuante ... :', num1);
+console.log('Number - Hexadecimal ... :', num2);
+console.log('Number - Octal ... :', num3);
+console.log('Number - Binário ... :', num4);
+// => Exemplo: Bigint
+let big1 = 2165465465465136n;
+let big2 = 1099511627779n;
+let big3 = 0x20000000000000003n;
+let big4 = 4611686018427387907n;
+console.log('Bigint ... :', big1);
+console.log('Bigint - Binário ... :', big2);
+console.log('Bigint - Hexadecimal ... :', big3);
+console.log('Bigint - Octal ... :', big4);
