ad6f9477e22b8744c417eddb1bad478c7db2865d
Jansen Muniz <92958989+Jansen-Muniz@users.noreply.github.com>
2023-05-11 21:47:03 -0300

    tipo string e tipo array

---
+++ modulo02/14-aula-string/stringType.js
@@ -0,0 +1,11 @@
+"use strict";
+// => Exemplo 01 - single quotes
+let nomeCompleto = 'Jansen Muniz';
+console.log(nomeCompleto);
+// => Exemplo 02 - double quotes
+let funcaoEmpresa = "Frontend developer";
+console.log(funcaoEmpresa);
+// => Exemplo 03 - back ticks
+let nomeEmpresa = 'Microsoft';
+let dadosFuncionario = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}`;
+console.log(dadosFuncionario);
---
+++ modulo02/14-aula-string/stringType.ts
@@ -0,0 +1,16 @@
+// => Exemplo 01 - single quotes
+
+let nomeCompleto: string = 'Jansen Muniz'
+console.log(nomeCompleto)
+
+// => Exemplo 02 - double quotes
+
+let funcaoEmpresa: string = "Frontend developer"
+console.log(funcaoEmpresa)
+
+// => Exemplo 03 - back ticks
+
+
+let nomeEmpresa: string = 'Microsoft'
+let dadosFuncionario: string = `Seja bem-vindo ${nomeCompleto}! Você será ${funcaoEmpresa} na empresa ${nomeEmpresa}`
+console.log(dadosFuncionario)
