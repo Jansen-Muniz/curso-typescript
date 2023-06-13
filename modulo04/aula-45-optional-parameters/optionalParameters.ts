d7d3e99e7a4322ffcb69261ef592d8a822e3acd4
Jansen Muniz <92958989+Jansen-Muniz@users.noreply.github.com>
2023-06-13 17:43:21 -0300

    optional parameters

---
+++ modulo04/aula-45-optional-parameters/optionalParameters.ts
@@ -0,0 +1,24 @@
+//exemplo 01 - optional parameters
+
+function informarDadosPessoa(idPessoa: number, nome: string, email?: string) {
+  console.log('Id Funcionário...', idPessoa, 'Nome...', nome)
+
+  if (email != undefined) console.log('Email...', email)
+}
+
+informarDadosPessoa(123, 'Jansen Muniz')
+informarDadosPessoa(456, 'Jôrdania Lima', 'jordania@email.com')
+
+//exemplo 02
+
+function mensagemLog(mensagem: string, usuarioId?: number) {
+  const horaLog = new Date().toLocaleTimeString()
+
+  console.log(horaLog, mensagem, usuarioId || 'Usuário(a) não conectado(a)')
+}
+
+mensagemLog('Atualizar página')
+mensagemLog('Usuário(a) logado(a) com sucesso', 123)
+
+//exemplo 03
+
